31aa55883fc203364be166dddc868d69cfb00dc4
Aasa-Christian <aasachristian@yahoo.com>
2023-05-02 17:47:22 -0400

    created redux boiler plate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { connect } from 'react-redux';
+import { actionCreator } from './Actions/Actions';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>React-Redux App</h1>
+        <button onClick={() => this.props.dispatch(actionCreator('Hello, Redux!'))}>
+          Click me!
+        </button>
+        <p>{this.props.message}</p>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  message: state.message
+});
+
+export default connect(mapStateToProps)(App);
